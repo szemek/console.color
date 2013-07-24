ca3bfa8adb8a456e8c1cb214b208b61551273731
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-07-24 12:12:24 +0200

    number of splits

--- js/panel.js
+++ js/panel.js
@@ -21,7 +21,7 @@ var COLORS = [
 chrome.experimental.devtools.console.onMessageAdded.addListener(function(message) {
   var template = $('#console-message').text();
 
-  var items = message.text.split(':');
+  var items = message.text.split(':', number_of_splits = 2);
   var color = items[0];
   var text = items[1];
 
