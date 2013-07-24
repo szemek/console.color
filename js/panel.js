33d89112965e9b63e1d5e387e6f7af5c7fb09819
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-07-24 12:18:24 +0200

    omit color prefix in log

--- js/panel.js
+++ js/panel.js
@@ -21,11 +21,11 @@ var COLORS = [
 chrome.experimental.devtools.console.onMessageAdded.addListener(function(message) {
   var template = $('#console-message').text();
 
-  var items = message.text.split(':', number_of_splits = 2);
+  var items = message.text.split(':');
   var color = items[0];
-  var text = items[1];
 
   if(COLORS.indexOf(color) != -1) {
+    var text = message.text.substr(message.text.indexOf(':') + 1);
     var item = template.replace('{{text}}', text).replace('{{color}}', color);
     $('.console-group-messages').append(item);
   }
